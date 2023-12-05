fd04a209cffcefa1c14257fba84ad2a2f541a36b
olwpad <olwpad@gmail.com>
2023-12-05 12:20:40 -0500

    Estilos de formulario

--- src/components/screens/RegisterScreen.js
+++ src/components/screens/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { View, Text } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 import { enviador } from '../../Functions/usePost';
 import { API_URLS } from '../../modulos/urls';
 
@@ -11,59 +12,42 @@ function Registrar() {
   const [email, setEmail] = useState('');
 
   const manejador = async () => {
-    try {
-      const respuesta = await enviador(API_URLS.Registrar, {
-        nombres,
-        apellidos,
-        usuario,
-        password,
-        email,
-      });
-  
-      if (respuesta && respuesta.ok) {
-        console.log('Registro exitoso');
-        Alert.alert('Registro Exitoso', 'Tu cuenta ha sido registrada exitosamente.');
-      } else {
-        console.error('Error en el registro',error);
-        Alert.alert('Error', 'Hubo un error en el registro. Por favor, inténtalo de nuevo.');
-      }
-    } catch (error) {
-      console.error('Error en la solicitud de registro:', error);
-      Alert.alert('Error de Red', 'Hubo un error en la solicitud de registro. Por favor, verifica tu conexión a internet.');
-    }
+    const respuesta = await enviador(API_URLS.Registrar, { nombres, apellidos, usuario, password, email });
   };
 
   return (
-    <View>
-      <Text>Regístrate</Text>
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 24, marginBottom: 16 }}>Regístrate</Text>
       <TextInput
-        placeholder="Nombres"
+        label="Nombres"
         value={nombres}
         onChangeText={(text) => setNombres(text)}
       />
       <TextInput
-        placeholder="Apellidos"
+        label="Apellidos"
         value={apellidos}
         onChangeText={(text) => setApellidos(text)}
       />
       <TextInput
-        placeholder="Email"
+        label="Email"
         value={email}
         onChangeText={(text) => setEmail(text)}
       />
       <TextInput
-        placeholder="Username"
+        label="Username"
         value={usuario}
         onChangeText={(text) => setUsuario(text)}
       />
       <TextInput
-        placeholder="Contraseña"
+        label="Contraseña"
         secureTextEntry
         value={password}
         onChangeText={(text) => setPassword(text)}
       />
-      <Button title="Regístrame" onPress={manejador} />
-      <Text>
+      <Button mode="contained" onPress={manejador} style={{ marginTop: 16 }}>
+        Regístrame
+      </Button>
+      <Text style={{ marginTop: 16 }}>
         ¿Ya tienes cuenta?{' '}
         <Text onPress={() => console.log('Navegar a Login')}>Inicia sesión</Text>
       </Text>
