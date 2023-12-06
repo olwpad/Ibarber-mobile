14d44e4c9bacb0b41ef6e08b69148e66d3260292
Condexd <camiloconde622@gmail.com>
2023-12-06 10:37:56 -0500

    Se actualizo RecuperarScreen

--- src/components/screens/RecuperarScreen.js
+++ src/components/screens/RecuperarScreen.js
@@ -1,8 +1,10 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text} from 'react-native';
 import { useForm } from '../../Hooks/useform';
 // import { API_URLS } from '../../modulos/urls';
 // import { enviador } from '../../Functions/usePost';
 import { useNavigation } from '@react-navigation/native';
+import { TextInput, Button } from 'react-native-paper';
+
 
 export const RecuperarScreen = () => {
   const { formState, funcion } = useForm({
@@ -16,7 +18,8 @@ export const RecuperarScreen = () => {
   };
 
   return (
-    <View>
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 24, marginBottom: 16 }}>Recuperar Cuenta</Text>
       <View>
         <Text>Olvidé mis datos de acceso</Text>
       </View>
@@ -28,9 +31,9 @@ export const RecuperarScreen = () => {
         />
       </View>
       <View>
-        <Button title="Enviar" onPress={manejador} />
+        <Button mode="contained" onPress={manejador} style={{ marginTop: 16 }}> Enviar </Button>
       </View>
-      <View>
+      <View style={{ marginTop: 16 }}>
         <Text>Te enviaremos una nueva contraseña y tu nombre de usuario</Text>
       </View>
     </View>
